7b86dcb27105772bca05dadbcd020296e4e33825
JAICHANGPARK <aristojeff@gmail.com>
2025-12-09 19:11:05 +0900

    Update e_commerce_home_page.dart

--- lib/ep2451_e_commerce_app/ui/e_commerce_home_page.dart
+++ lib/ep2451_e_commerce_app/ui/e_commerce_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ECommerceHomePage extends StatefulWidget {
   const ECommerceHomePage({super.key});
 
@@ -12,13 +11,24 @@ class _ECommerceHomePageState extends State<ECommerceHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-          child: Column(
-            children: [
-              
-            ],
-          ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+                ExcludeFocus(
+                  child: Center(
+                    child: Container(
+                      decoration: BoxDecoration(color: Colors.black),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
+      ),
     );
   }
 }
