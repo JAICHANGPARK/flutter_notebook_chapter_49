47b78cb61e49e96a8876bcc80092bec354555308
JAICHANGPARK <aristojeff@gmail.com>
2025-12-11 21:04:04 +0900

    Update e_commerce_checkout_page.dart

--- lib/ep2451_e_commerce_app/ui/e_commerce_checkout_page.dart
+++ lib/ep2451_e_commerce_app/ui/e_commerce_checkout_page.dart
@@ -12,7 +12,14 @@ class _ECommerceCheckoutPageState extends State<ECommerceCheckoutPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: .fromRGBO(238, 238, 238, 1),
-
+      body: SafeArea(child: Column(children: [
+        Row(
+          children: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back_ios_new_outlined)),
+            Expanded(child: Center(child: Text("Checkout"),)),
+          ],
+        )
+      ],)),
     );
   }
 }
