bb51386f47780563a17a939d8e3b4756c7fd1b2c
JAICHANGPARK <aristojeff@gmail.com>
2025-12-19 19:46:56 +0900

    Create furniture_detail_page.dart

---
+++ lib/ep2460_furniture_app/ui/furniture_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FurnitureDetailPage extends StatefulWidget {
+  const FurnitureDetailPage({super.key});
+
+  @override
+  State<FurnitureDetailPage> createState() => _FurnitureDetailPageState();
+}
+
+class _FurnitureDetailPageState extends State<FurnitureDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
