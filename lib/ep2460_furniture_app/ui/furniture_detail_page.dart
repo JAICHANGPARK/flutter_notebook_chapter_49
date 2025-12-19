1eae8a9b863271cae6cd133083ad0d8472e63236
JAICHANGPARK <aristojeff@gmail.com>
2025-12-19 19:53:36 +0900

    Update furniture_detail_page.dart

--- lib/ep2460_furniture_app/ui/furniture_detail_page.dart
+++ lib/ep2460_furniture_app/ui/furniture_detail_page.dart
@@ -67,7 +67,16 @@ class _FurnitureDetailPageState extends State<FurnitureDetailPage> {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             maxLines: 3,
           ),
-          
+          Text("Colors"),
+          SizedBox(height: 52, child: Placeholder()),
+          Row(
+            children: [
+              Column(children: [Text("\$12345"), Text("\$1200")]),
+              Expanded(child: Container(decoration: BoxDecoration(
+                color: Colors.black
+              ),),)
+            ],
+          ),
         ],
       ),
     );
