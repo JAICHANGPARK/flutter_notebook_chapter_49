f87b0ce6a38cbb1d1031a95a6dbe1f62a4df9285
JAICHANGPARK <aristojeff@gmail.com>
2025-12-19 19:24:10 +0900

    Update furniture_table_page.dart

--- lib/ep2460_furniture_app/ui/furniture_table_page.dart
+++ lib/ep2460_furniture_app/ui/furniture_table_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class FurnitureTablePage extends StatefulWidget {
+  const FurnitureTablePage({super.key});
+
+  @override
+  State<FurnitureTablePage> createState() => _FurnitureTablePageState();
+}
+
+class _FurnitureTablePageState extends State<FurnitureTablePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
