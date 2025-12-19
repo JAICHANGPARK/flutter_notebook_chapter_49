cfab9e526d903b04b813ef10e3b8a2b3b5e9f06b
JAICHANGPARK <aristojeff@gmail.com>
2025-12-19 19:52:39 +0900

    Update furniture_detail_page.dart

--- lib/ep2460_furniture_app/ui/furniture_detail_page.dart
+++ lib/ep2460_furniture_app/ui/furniture_detail_page.dart
@@ -45,7 +45,26 @@ class _FurnitureDetailPageState extends State<FurnitureDetailPage> {
                 ),
                 child: Row(children: [Icon(Icons.star), Text("4.8 Ratings")]),
               ),
+              Container(
+                decoration: ShapeDecoration(
+                  shape: StadiumBorder(
+                    side: BorderSide(color: Colors.grey[400]!),
+                  ),
+                ),
+                child: Row(children: [Text("3.2K+ Reviews")]),
+              ),
+              Container(
+                decoration: ShapeDecoration(
+                  shape: StadiumBorder(
+                    side: BorderSide(color: Colors.grey[400]!),
+                  ),
+                ),
+                child: Row(children: [Text("13K Sold")]),
+              ),
             ],
+          ),
+          Text(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
           ),
         ],
       ),
