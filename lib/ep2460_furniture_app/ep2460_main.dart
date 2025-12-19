eb1e34e2a54b24fdb8be2a49e8318fbae79c61a3
JAICHANGPARK <aristojeff@gmail.com>
2025-12-19 19:24:22 +0900

    Update ep2460_main.dart

--- lib/ep2460_furniture_app/ep2460_main.dart
+++ lib/ep2460_furniture_app/ep2460_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_49/ep2460_furniture_app/ui/furniture_app.dart';
 
 
 void main() {
-  
+  runApp(FurnitureApp());
 }
