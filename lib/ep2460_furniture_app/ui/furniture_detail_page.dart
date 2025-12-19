aaae943ef698233d2a34c8eb370f487a8844acaf
JAICHANGPARK <aristojeff@gmail.com>
2025-12-19 20:07:44 +0900

    Update furniture_detail_page.dart

--- lib/ep2460_furniture_app/ui/furniture_detail_page.dart
+++ lib/ep2460_furniture_app/ui/furniture_detail_page.dart
@@ -26,110 +26,126 @@ class _FurnitureDetailPageState extends State<FurnitureDetailPage> {
             decoration: BoxDecoration(color: Colors.grey[100]),
           ),
           Gap(16),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              crossAxisAlignment: .start,
-              spacing: 16,
-              children: [
-                Row(
-                  spacing: 12,
-                  children: [
-                    Text("Square stool", style: TextStyle(fontSize: 24)),
-                    Container(
-                      padding: .symmetric(horizontal: 12, vertical: 4),
-                      decoration: ShapeDecoration(
-                        shape: StadiumBorder(),
-                        color: Colors.black,
-                      ),
-                      child: Text(
-                        "15% off",
-                        style: TextStyle(color: Colors.white, fontSize: 12),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 32,
-                  child: Row(
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: .start,
+                spacing: 16,
+                children: [
+                  Row(
                     spacing: 12,
                     children: [
+                      Text("Square stool", style: TextStyle(fontSize: 24)),
                       Container(
-                        height: 32,
-                        padding: .symmetric(horizontal: 12),
+                        padding: .symmetric(horizontal: 12, vertical: 4),
                         decoration: ShapeDecoration(
-                          shape: StadiumBorder(
-                            side: BorderSide(color: Colors.grey[400]!),
-                          ),
+                          shape: StadiumBorder(),
+                          color: Colors.black,
                         ),
-                        child: Row(
-                          spacing: 5,
-                          children: [
-                            Icon(Icons.star, size: 18),
-                            Text("4.8 Ratings"),
-                          ],
+                        child: Text(
+                          "15% off",
+                          style: TextStyle(color: Colors.white, fontSize: 12),
                         ),
-                      ),
-                      Container(
-                        height: 32,
-                        padding: .symmetric(horizontal: 12),
-                        decoration: ShapeDecoration(
-                          shape: StadiumBorder(
-                            side: BorderSide(color: Colors.grey[400]!),
-                          ),
-                        ),
-                        child: Row(children: [Text("3.2K+ Reviews")]),
-                      ),
-                      Container(
-                        height: 32,
-                        padding: .symmetric(horizontal: 12),
-                        decoration: ShapeDecoration(
-                          shape: StadiumBorder(
-                            side: BorderSide(color: Colors.grey[400]!),
-                          ),
-                        ),
-                        child: Row(children: [Text("13K Sold")]),
                       ),
                     ],
                   ),
-                ),
-                Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                  maxLines: 3,
-                  style: TextStyle(fontSize: 12),
-                ),
-                Text(
-                  "Colors",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                ),
-                SizedBox(height: 52, child: Placeholder()),
-                Row(
-                  children: [
-                    Column(
+                  SizedBox(
+                    height: 32,
+                    child: Row(
+                      spacing: 12,
+                      children: [
+                        Container(
+                          height: 32,
+                          padding: .symmetric(horizontal: 12),
+                          decoration: ShapeDecoration(
+                            shape: StadiumBorder(
+                              side: BorderSide(color: Colors.grey[400]!),
+                            ),
+                          ),
+                          child: Row(
+                            spacing: 5,
+                            children: [
+                              Icon(Icons.star, size: 18),
+                              Text("4.8 Ratings"),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          height: 32,
+                          padding: .symmetric(horizontal: 12),
+                          decoration: ShapeDecoration(
+                            shape: StadiumBorder(
+                              side: BorderSide(color: Colors.grey[400]!),
+                            ),
+                          ),
+                          child: Row(children: [Text("3.2K+ Reviews")]),
+                        ),
+                        Container(
+                          height: 32,
+                          padding: .symmetric(horizontal: 12),
+                          decoration: ShapeDecoration(
+                            shape: StadiumBorder(
+                              side: BorderSide(color: Colors.grey[400]!),
+                            ),
+                          ),
+                          child: Row(children: [Text("13K Sold")]),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    maxLines: 3,
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  Text(
+                    "Colors",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  SizedBox(height: 52, child: Placeholder()),
+                  Spacer(),
+            
+                  Row(
+                    children: [
+                      Column(
                         crossAxisAlignment: .start,
-                        children: [Text("\$12345"), Text("\$1200")]),
-                    Spacer(),
-                    Container(
-                      padding: .symmetric(horizontal: 52, vertical: 8),
-                      decoration: BoxDecoration(color: Colors.black),
-                      child: Row(
-                        mainAxisAlignment: .center,
-
+                        spacing: 6,
                         children: [
-                          Icon(
-                            Icons.shopping_bag_outlined,
-                            color: Colors.white,
-                          ),
-                          Text(
-                            "Add to cart",
-                            style: TextStyle(color: Colors.white),
-                          ),
+                          Text("\$1234",style: TextStyle(
+                            fontSize: 12,
+                          ),),
+                          Text("\$1200", style: TextStyle(fontWeight: .bold,
+                          fontSize: 20)),
                         ],
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      Spacer(),
+                      Container(
+                        padding: .symmetric(horizontal: 52, vertical: 8),
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: .circular(8),
+                        ),
+                        child: Row(
+                          spacing: 6,
+                          mainAxisAlignment: .center,
+            
+                          children: [
+                            Icon(
+                              Icons.shopping_bag_outlined,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              "Add to cart",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
