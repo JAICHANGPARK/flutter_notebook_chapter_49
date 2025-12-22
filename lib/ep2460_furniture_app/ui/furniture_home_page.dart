b06dc0aa87a11c53c1ba05385ec226804997ba29
JAICHANGPARK <aristojeff@gmail.com>
2025-12-22 19:58:57 +0900

    Update furniture_home_page.dart

--- lib/ep2460_furniture_app/ui/furniture_home_page.dart
+++ lib/ep2460_furniture_app/ui/furniture_home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_49/ep2460_furniture_app/ui/furniture_table_page.dart';
 
-
 class FurnitureHomePage extends StatefulWidget {
   const FurnitureHomePage({super.key});
 
@@ -11,19 +10,34 @@ class FurnitureHomePage extends StatefulWidget {
 
 class _FurnitureHomePageState extends State<FurnitureHomePage> {
   int pageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: IndexedStack(
-        index:  pageIndex,
+        index: pageIndex,
         children: [
           FurnitureTablePage(),
           Container(),
           Container(),
-          Placeholder()
+          Placeholder(),
         ],
       ),
-      
+      bottomNavigationBar: Container(
+        height: 88,
+        child: Row(
+          children: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.home_outlined)),
+            Container(
+              decoration: ShapeDecoration(
+                shape: StadiumBorder(),
+                color: Colors.black,
+              ),
+            ),
+            IconButton(onPressed: (){}, icon: Icon(Icons.shopping_cart_outlined)),
+          ],
+        ),
+      ),
     );
   }
 }
