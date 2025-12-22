6001a22ff82c486df42ae6bcf26c7bc110aaf4b7
JAICHANGPARK <aristojeff@gmail.com>
2025-12-22 20:10:01 +0900

    Update furniture_cart_page.dart

--- lib/ep2460_furniture_app/ui/furniture_cart_page.dart
+++ lib/ep2460_furniture_app/ui/furniture_cart_page.dart
@@ -12,7 +12,17 @@ class _FurnitureCartPageState extends State<FurnitureCartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back)),
+        actions: [IconButton(onPressed: (){}, icon: Icon(Icons.search))],
+        centerTitle: true,
+        title: Text("My cart"),
+      ),
+      body: Column(
+        children: [
+
+        ],
+      ),
     );
   }
 }
