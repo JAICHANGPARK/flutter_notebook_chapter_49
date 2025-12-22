7b11d45274f30e839248c11dea60ba7c2d9bf3bd
JAICHANGPARK <aristojeff@gmail.com>
2025-12-22 19:57:31 +0900

    Update furniture_app.dart

--- lib/ep2460_furniture_app/ui/furniture_app.dart
+++ lib/ep2460_furniture_app/ui/furniture_app.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_49/ep2460_furniture_app/ui/furniture_table_page.dart';
 
+import 'furniture_home_page.dart';
+
 class FurnitureApp extends StatelessWidget {
   const FurnitureApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FurnitureTablePage(),
+      home: FurnitureHomePage(),
     );
   }
 }
