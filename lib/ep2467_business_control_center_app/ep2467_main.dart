e8749c78aa07b903d0a6419aa763b76393b7f763
JAICHANGPARK <aristojeff@gmail.com>
2025-12-24 10:15:30 +0900

    Update ep2467_main.dart

--- lib/ep2467_business_control_center_app/ep2467_main.dart
+++ lib/ep2467_business_control_center_app/ep2467_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_49/ep2467_business_control_center_app/business_control_center_app.dart';
 
 
 void main() {
-  
+  runApp(BusinessControlCenterApp());
 }
