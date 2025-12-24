b2ffa51491b2ebeff2c2acf0db48175ab2e3452a
JAICHANGPARK <aristojeff@gmail.com>
2025-12-24 10:22:02 +0900

    Update business_main_page.dart

--- lib/ep2467_business_control_center_app/ui/business_main_page.dart
+++ lib/ep2467_business_control_center_app/ui/business_main_page.dart
@@ -11,11 +11,30 @@ class _BusinessMainPageState extends State<BusinessMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Business"),
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.menu))],
       ),
       body: Column(children: []),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey[400]!,
+              blurRadius: 10,
+              spreadRadius: 30
+            )
+          ],
+        ),
+
+        child: Row(
+          children: [
+
+          ],
+        ),
+      ),
     );
   }
 }
