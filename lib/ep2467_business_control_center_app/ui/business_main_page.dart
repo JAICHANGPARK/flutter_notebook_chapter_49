838d23b636a9c6d3f665582873ebeca01c53dfc0
JAICHANGPARK <aristojeff@gmail.com>
2025-12-24 10:15:03 +0900

    Create business_main_page.dart

---
+++ lib/ep2467_business_control_center_app/ui/business_main_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class BusinessMainPage extends StatefulWidget {
+  const BusinessMainPage({super.key});
+
+  @override
+  State<BusinessMainPage> createState() => _BusinessMainPageState();
+}
+
+class _BusinessMainPageState extends State<BusinessMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
