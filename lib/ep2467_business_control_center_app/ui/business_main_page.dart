c50814984570fbfa167c769525d4a629a95b47cf
JAICHANGPARK <aristojeff@gmail.com>
2025-12-25 10:01:54 +0900

    Update business_main_page.dart

--- lib/ep2467_business_control_center_app/ui/business_main_page.dart
+++ lib/ep2467_business_control_center_app/ui/business_main_page.dart
@@ -35,7 +35,7 @@ class _BusinessMainPageState extends State<BusinessMainPage> {
                         padding: EdgeInsets.all(4),
                         decoration: BoxDecoration(
                           color: Colors.white,
-                          borderRadius: BorderRadius.circular(3),
+                          borderRadius: BorderRadius.circular(6),
                           boxShadow: [
                             BoxShadow(
                               color: Colors.grey[300]!,
@@ -50,7 +50,7 @@ class _BusinessMainPageState extends State<BusinessMainPage> {
                         padding: EdgeInsets.all(4),
                         decoration: BoxDecoration(
                           color: Colors.white,
-                          borderRadius: BorderRadius.circular(3),
+                          borderRadius: BorderRadius.circular(6),
                           boxShadow: [
                             BoxShadow(
                               color: Colors.grey[300]!,
