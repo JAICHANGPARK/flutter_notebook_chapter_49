b09c90212865c9a595d2abda3841d9da5698b830
JAICHANGPARK <aristojeff@gmail.com>
2025-12-26 10:35:53 +0900

    Update business_traffic_card_widget.dart

--- lib/ep2467_business_control_center_app/ui/business_traffic_card_widget.dart
+++ lib/ep2467_business_control_center_app/ui/business_traffic_card_widget.dart
@@ -10,6 +10,7 @@ class BusinessTrafficCardWidget extends StatelessWidget {
       child: Column(
         children: [
           Row(
+            spacing: 16,
             children: [
               Container(
                 padding: EdgeInsets.all(10),
@@ -20,6 +21,12 @@ class BusinessTrafficCardWidget extends StatelessWidget {
                 child: Icon(Icons.traffic_outlined,
                 color: Color(0xFFFF6B00),),
               ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+              ],)
+
             ],
           ),
         ],
