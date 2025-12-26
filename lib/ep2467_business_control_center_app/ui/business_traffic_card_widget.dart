f4b22f667a4bbfe60f4737ea4d47a8182934f9b1
JAICHANGPARK <aristojeff@gmail.com>
2025-12-26 10:35:23 +0900

    Create business_traffic_card_widget.dart

---
+++ lib/ep2467_business_control_center_app/ui/business_traffic_card_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BusinessTrafficCardWidget extends StatelessWidget {
+  const BusinessTrafficCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Color(0xFFFFF0E0),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Icon(Icons.traffic_outlined,
+                color: Color(0xFFFF6B00),),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
