43a1c119a94629d46faff607aec2884c01e28b26
JAICHANGPARK <aristojeff@gmail.com>
2025-12-27 10:25:48 +0900

    Update business_traffic_card_widget.dart

--- lib/ep2467_business_control_center_app/ui/business_traffic_card_widget.dart
+++ lib/ep2467_business_control_center_app/ui/business_traffic_card_widget.dart
@@ -147,6 +147,15 @@ class PlatformRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        Container(
+          width: 10,
+          height: 10,
+          decoration: BoxDecoration(color: color,
+          shape: BoxShape.circle),
+        ),
+      ],
+    );
   }
 }
