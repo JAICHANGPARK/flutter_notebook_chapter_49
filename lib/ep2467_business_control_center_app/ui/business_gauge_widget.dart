8d6e17dcd074ce727ca45c5e55cda66a929ea6b6
JAICHANGPARK <aristojeff@gmail.com>
2025-12-28 10:56:14 +0900

    Update business_gauge_widget.dart

---
+++ lib/ep2467_business_control_center_app/ui/business_gauge_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class BusinessGaugeWidget extends StatelessWidget {
+  const BusinessGaugeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
