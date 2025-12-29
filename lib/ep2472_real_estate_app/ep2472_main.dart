55910dde38f8879124859d0c2639d62ee5b6f14b
JAICHANGPARK <aristojeff@gmail.com>
2025-12-29 09:27:08 +0900

    Update ep2472_main.dart

--- lib/ep2472_real_estate_app/ep2472_main.dart
+++ lib/ep2472_real_estate_app/ep2472_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_49/ep2472_real_estate_app/real_estate_biz_app.dart';
 
 
 void main() {
-  
+  runApp(RealEstateBizApp());
 }
