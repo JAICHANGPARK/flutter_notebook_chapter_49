ed5e8db9a26bd2c6a902b43eec48e68fb53ec5fb
JAICHANGPARK <aristojeff@gmail.com>
2025-12-29 09:25:51 +0900

    Update real_estate_biz_app.dart

---
+++ lib/ep2472_real_estate_app/real_estate_biz_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class RealEstateBizApp extends StatelessWidget {
+  const RealEstateBizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
