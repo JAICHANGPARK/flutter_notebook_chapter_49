2b5e2824a3e9624f6a0009a9e849e8086eee835d
JAICHANGPARK <aristojeff@gmail.com>
2026-01-02 19:48:57 +0900

    Update real_estate_detail_page.dart

--- lib/ep2472_real_estate_app/ui/real_estate_detail_page.dart
+++ lib/ep2472_real_estate_app/ui/real_estate_detail_page.dart
@@ -93,14 +93,16 @@ class _RealEstateDetailPageState extends State<RealEstateDetailPage> {
                     Divider(),
                     Text(
                       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
-
                       maxLines: 3,
                     ),
                     SizedBox(
                       height: 120,
                       child: ListView.builder(
                         itemBuilder: (context, index) {
-                          return Container();
+                          return Container(
+                            width: 140,
+                            decoration: BoxDecoration(color: Colors.white),
+                          );
                         },
                       ),
                     ),
