c2af14eeb4380b924a6d415dc7d30f14dadebe5b
JAICHANGPARK <aristojeff@gmail.com>
2026-01-02 19:44:18 +0900

    Update real_estate_detail_page.dart

--- lib/ep2472_real_estate_app/ui/real_estate_detail_page.dart
+++ lib/ep2472_real_estate_app/ui/real_estate_detail_page.dart
@@ -11,6 +11,42 @@ class RealEstateDetailPage extends StatefulWidget {
 class _RealEstateDetailPageState extends State<RealEstateDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(height: 320, child: Placeholder()),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
+                    children: [Text("Unique 3 Bedroom Flat"), Text("\$449.00")],
+                  ),
+                  Row(
+                    spacing: 12,
+                    children: [
+                      CircleAvatar(),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Posted by Dream"),
+                            Row(
+                              children: [
+                                Icon(Icons.star, color: Colors.deepOrange),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
