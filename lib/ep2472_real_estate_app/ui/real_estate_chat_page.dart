a0b3a726e800c3f87151edce9d640ed68a6dfc16
JAICHANGPARK <aristojeff@gmail.com>
2026-01-04 19:11:13 +0900

    Create real_estate_chat_page.dart

---
+++ lib/ep2472_real_estate_app/ui/real_estate_chat_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class RealEstateChatPage extends StatefulWidget {
+  const RealEstateChatPage({super.key});
+
+  @override
+  State<RealEstateChatPage> createState() => _RealEstateChatPageState();
+}
+
+class _RealEstateChatPageState extends State<RealEstateChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
