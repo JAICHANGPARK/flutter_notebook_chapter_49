9e4afe1fe184070772b99e0e23dbcc4f499fec4b
JAICHANGPARK <aristojeff@gmail.com>
2026-01-04 19:12:03 +0900

    Update real_estate_chat_page.dart

--- lib/ep2472_real_estate_app/ui/real_estate_chat_page.dart
+++ lib/ep2472_real_estate_app/ui/real_estate_chat_page.dart
@@ -11,6 +11,6 @@ class RealEstateChatPage extends StatefulWidget {
 class _RealEstateChatPageState extends State<RealEstateChatPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
