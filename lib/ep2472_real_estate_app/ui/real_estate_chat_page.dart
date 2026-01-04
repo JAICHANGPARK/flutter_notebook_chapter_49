29cb77b1c897058ae1e8cb8a227ef941f08231aa
JAICHANGPARK <aristojeff@gmail.com>
2026-01-04 19:15:19 +0900

    Update real_estate_chat_page.dart

--- lib/ep2472_real_estate_app/ui/real_estate_chat_page.dart
+++ lib/ep2472_real_estate_app/ui/real_estate_chat_page.dart
@@ -12,17 +12,21 @@ class _RealEstateChatPageState extends State<RealEstateChatPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100]!,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(height: 80, child: Placeholder()),
-            Container(
-              height: 120,
-              child: Placeholder(),
-            ),
-            Expanded(child: Placeholder()),
-            Container(height: 80, child: Placeholder()),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SafeArea(
+          bottom: false,
+          child: Column(
+            children: [
+              Container(height: 80, child: Placeholder()),
+              Container(
+                height: 120,
+                child: Placeholder(),
+              ),
+              Expanded(child: Placeholder()),
+              Container(height: 80, child: Placeholder()),
+            ],
+          ),
         ),
       ),
     );
