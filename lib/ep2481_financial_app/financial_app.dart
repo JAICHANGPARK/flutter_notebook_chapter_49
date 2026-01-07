ddcac01bbb15937ff3627fd72ad6ece258025108
JAICHANGPARK <aristojeff@gmail.com>
2026-01-07 18:20:01 +0900

    Update financial_app.dart

---
+++ lib/ep2481_financial_app/financial_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class FinancialApp extends StatelessWidget {
+  const FinancialApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
