6e2bc34497d7698dd333357d23fb1aba020ca5fb
JAICHANGPARK <aristojeff@gmail.com>
2026-01-07 18:20:39 +0900

    Update ep2481_main.dart

--- lib/ep2481_financial_app/ep2481_main.dart
+++ lib/ep2481_financial_app/ep2481_main.dart
@@ -1,5 +1,8 @@
 
 
-void main() {
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_notebook_chapter_49/ep2481_financial_app/financial_app.dart';
 
+void main() {
+    runApp(FinancialApp());
 }
