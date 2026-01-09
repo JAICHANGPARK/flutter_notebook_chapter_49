0c0758f5f0d30f4e38ec9db370d20a87d02e36ac
JAICHANGPARK <aristojeff@gmail.com>
2026-01-09 17:56:26 +0900

    Update finance_custom_page.dart

---
+++ lib/ep2481_financial_app/ui/finance_custom_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FinanceCustomPage extends StatefulWidget {
+  const FinanceCustomPage({super.key});
+
+  @override
+  State<FinanceCustomPage> createState() => _FinanceCustomPageState();
+}
+
+class _FinanceCustomPageState extends State<FinanceCustomPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
