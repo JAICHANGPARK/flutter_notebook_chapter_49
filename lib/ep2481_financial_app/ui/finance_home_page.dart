c8cc0777564debd86427ccb346a0897028fce2e1
JAICHANGPARK <aristojeff@gmail.com>
2026-01-09 18:03:53 +0900

    Update finance_home_page.dart

--- lib/ep2481_financial_app/ui/finance_home_page.dart
+++ lib/ep2481_financial_app/ui/finance_home_page.dart
@@ -10,102 +10,114 @@ class FinanceHomePage extends StatefulWidget {
 class _FinanceHomePageState extends State<FinanceHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      spacing: 16,
-      children: [
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Container(
-            height: 58,
-            margin: EdgeInsets.only(left: 12),
-            child: Row(
-              children: [
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  height: double.infinity,
-
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(229, 235, 240, 1),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(32),
-                      bottomLeft: Radius.circular(32),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 16,
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Container(
+              height: 58,
+              margin: EdgeInsets.only(left: 12),
+              child: Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    height: double.infinity,
+      
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(229, 235, 240, 1),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(32),
+                        bottomLeft: Radius.circular(32),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Accounts",
+                        style: TextStyle(color: Colors.black38, fontSize: 16),
+                      ),
                     ),
                   ),
-                  child: Center(
-                    child: Text(
-                      "Accounts",
-                      style: TextStyle(color: Colors.black38, fontSize: 16),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    height: double.infinity,
+                    decoration: BoxDecoration(color: Colors.white),
+                    child: Center(
+                      child: Text(
+                        "Cards",
+                        style: TextStyle(color: Colors.black, fontSize: 16),
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  height: double.infinity,
-                  decoration: BoxDecoration(color: Colors.white),
-                  child: Center(
-                    child: Text(
-                      "Cards",
-                      style: TextStyle(color: Colors.black, fontSize: 16),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(229, 235, 240, 1),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Linked",
+                        style: TextStyle(color: Colors.black38, fontSize: 16),
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(229, 235, 240, 1),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Linked",
-                      style: TextStyle(color: Colors.black38, fontSize: 16),
+                  VerticalDivider(color: Colors.white, width: 1),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(229, 235, 240, 1),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Stocks",
+                        style: TextStyle(color: Colors.black38, fontSize: 16),
+                      ),
                     ),
                   ),
-                ),
-                VerticalDivider(color: Colors.white, width: 1),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(229, 235, 240, 1),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Stocks",
-                      style: TextStyle(color: Colors.black38, fontSize: 16),
+                  VerticalDivider(color: Colors.white, width: 1),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(229, 235, 240, 1),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Stocks",
+                        style: TextStyle(color: Colors.black38, fontSize: 16),
+                      ),
                     ),
                   ),
-                ),
-                VerticalDivider(color: Colors.white, width: 1),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(229, 235, 240, 1),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Stocks",
-                      style: TextStyle(color: Colors.black38, fontSize: 16),
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 16),
-          height: 260,
-          child: Placeholder(),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 16),
-          height: 140,
-          child: Placeholder(),
-        ),
-      ],
+          Column(
+            spacing: 16,
+            children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              height: 280,
+              child: Placeholder(),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              height: 140,
+              child: Placeholder(),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              height: 140,
+              child: Placeholder(),
+            ),
+          ],)
+
+        ],
+      ),
     );
   }
 }
