ddd3a8265eceb8908e9032bbc9dab2f14b425684
JAICHANGPARK <aristojeff@gmail.com>
2026-01-11 09:59:05 +0900

    Update finance_custom_page.dart

--- lib/ep2481_financial_app/ui/finance_custom_page.dart
+++ lib/ep2481_financial_app/ui/finance_custom_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FinanceCustomPage extends StatefulWidget {
   const FinanceCustomPage({super.key});
 
@@ -15,24 +14,25 @@ class _FinanceCustomPageState extends State<FinanceCustomPage> {
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              height: 420,
-              child: Placeholder(),
-            ),
+            Container(height: 420, child: Placeholder()),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text("Customize your Home screen"),
-                IconButton(onPressed: (){}, icon: Icon(Icons.clear))
+                IconButton(onPressed: () {}, icon: Icon(Icons.clear)),
               ],
             ),
             Text("Long press and drag widgets to reorder"),
-            TextButton(onPressed: (){}, child: Text("Learn more"),),
+            TextButton(
+              onPressed: () {},
+              child: Text("Learn more"),
+              style: TextButton.styleFrom(padding: .zero),
+            ),
           ],
         ),
-      )
-
+      ),
     );
   }
 }
