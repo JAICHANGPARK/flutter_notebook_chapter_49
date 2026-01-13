26a71de41f90d4ad99675d7ed741048a0ed01597
JAICHANGPARK <aristojeff@gmail.com>
2026-01-13 18:43:16 +0900

    Update running_main_page.dart

--- lib/ep2486_running_app/ui/running_main_page.dart
+++ lib/ep2486_running_app/ui/running_main_page.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
 
+class RunningMainPage extends StatefulWidget {
+  const RunningMainPage({super.key});
+
+  @override
+  State<RunningMainPage> createState() => _RunningMainPageState();
+}
+
+class _RunningMainPageState extends State<RunningMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
