a9e3926b4749e30102211a78941c2c300ebe2e58
JAICHANGPARK <aristojeff@gmail.com>
2026-01-13 18:52:19 +0900

    Update running_main_page.dart

--- lib/ep2486_running_app/ui/running_main_page.dart
+++ lib/ep2486_running_app/ui/running_main_page.dart
@@ -11,14 +11,21 @@ class _RunningMainPageState extends State<RunningMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: [BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.map), label: "Maps"),
-          BottomNavigationBarItem(icon: Icon(Icons.play_arrow_outlined), label: "Record"),
-          BottomNavigationBarItem(icon: Icon(Icons.group_add_outlined), label: "Groups"),
-          BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: "Profile"),
-        ],
+      backgroundColor: Color.fromRGBO(239, 239, 239, 1),
+      bottomNavigationBar: SizedBox(
+        height: 100,
+
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+
+          type: BottomNavigationBarType.fixed,
+          items: [BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.map), label: "Maps"),
+            BottomNavigationBarItem(icon: Icon(Icons.play_arrow_outlined), label: "Record"),
+            BottomNavigationBarItem(icon: Icon(Icons.group_add_outlined), label: "Groups"),
+            BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: "Profile"),
+          ],
+        ),
       ),
     );
   }
