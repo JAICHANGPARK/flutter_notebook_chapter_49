22f60bc3d6e80c1c95939a60d6c79419ee2d7c2e
JAICHANGPARK <aristojeff@gmail.com>
2026-01-13 18:46:38 +0900

    Update running_main_page.dart

--- lib/ep2486_running_app/ui/running_main_page.dart
+++ lib/ep2486_running_app/ui/running_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class RunningMainPage extends StatefulWidget {
   const RunningMainPage({super.key});
 
@@ -11,6 +10,15 @@ class RunningMainPage extends StatefulWidget {
 class _RunningMainPageState extends State<RunningMainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Maps"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Record"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Groups"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Profile"),
+        ],
+      ),
+    );
   }
 }
