57cf5a7b13523399b80a9b2bbf5ea344d54df95a
JAICHANGPARK <aristojeff@gmail.com>
2026-01-13 18:44:35 +0900

    Update running_app.dart

---
+++ lib/ep2486_running_app/running_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_49/ep2486_running_app/ui/running_main_page.dart';
+
+
+class RunningApp extends StatelessWidget {
+  const RunningApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: RunningMainPage(),
+    );
+  }
+}
