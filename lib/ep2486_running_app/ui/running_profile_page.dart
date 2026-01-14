2fde9f6ea10375c0d878962069572fbc7f6de9bf
JAICHANGPARK <aristojeff@gmail.com>
2026-01-14 18:48:40 +0900

    Update running_profile_page.dart

--- lib/ep2486_running_app/ui/running_profile_page.dart
+++ lib/ep2486_running_app/ui/running_profile_page.dart
@@ -18,8 +18,33 @@ class _RunningProfilePageState extends State<RunningProfilePage> {
             Row(
               children: [
                 CircleAvatar(),
-                Expanded(child: Center(child: Text("Profile"))),
-                CircleAvatar(),
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      "Profile",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Container(
+                  
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey[100]!,
+                        blurRadius: 3,
+                        spreadRadius: 3,
+                      ),
+                    ],
+                  ),
+                  child: Icon(Icons.settings_outlined),
+                ),
               ],
             ),
           ],
