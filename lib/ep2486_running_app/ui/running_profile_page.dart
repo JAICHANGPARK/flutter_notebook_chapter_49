910536d72cce7758bb6ab9c44f5e30ecf0e3561d
JAICHANGPARK <aristojeff@gmail.com>
2026-01-14 18:46:48 +0900

    Update running_profile_page.dart

--- lib/ep2486_running_app/ui/running_profile_page.dart
+++ lib/ep2486_running_app/ui/running_profile_page.dart
@@ -12,7 +12,7 @@ class _RunningProfilePageState extends State<RunningProfilePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
         child: Column(
           children: [
             Row(
