207fd6c9a91ed45e1a99f13b63d10404e98b386c
JAICHANGPARK <aristojeff@gmail.com>
2026-01-17 09:23:35 +0900

    Update running_profile_page.dart

--- lib/ep2486_running_app/ui/running_profile_page.dart
+++ lib/ep2486_running_app/ui/running_profile_page.dart
@@ -14,7 +14,7 @@ class _RunningProfilePageState extends State<RunningProfilePage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    _scrollController = ScrollController(initialScrollOffset: 0.0);
+    _scrollController = ScrollController(initialScrollOffset: 0.5);
 
   }
   @override
