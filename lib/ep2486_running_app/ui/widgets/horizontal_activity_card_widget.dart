6014955bf60f23905d648ddf08d0dc8474391edc
JAICHANGPARK <aristojeff@gmail.com>
2026-01-17 09:27:21 +0900

    Update horizontal_activity_card_widget.dart

--- lib/ep2486_running_app/ui/widgets/horizontal_activity_card_widget.dart
+++ lib/ep2486_running_app/ui/widgets/horizontal_activity_card_widget.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class HorizontalActivityCardWidget extends StatefulWidget {
   const HorizontalActivityCardWidget({super.key});
 
@@ -6,8 +8,40 @@ class HorizontalActivityCardWidget extends StatefulWidget {
 }
 
 class _HorizontalActivityCardWidgetState extends State<HorizontalActivityCardWidget> {
+
+  ScrollController? _scrollController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _scrollController = ScrollController(initialScrollOffset: 0);
+    _scrollController?.addListener((){
+      print("${_scrollController?.offset}");
+    });
+
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _scrollController?.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      controller: _scrollController,
+      itemBuilder: (context, index) {
+        return Container(
+          width: 180,
+          margin: EdgeInsets.symmetric(horizontal: 4),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: .circular(16),
+          ),
+        );
+      },
+    );
   }
 }
