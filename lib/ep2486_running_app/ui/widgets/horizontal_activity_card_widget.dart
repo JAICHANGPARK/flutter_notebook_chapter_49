442ae894007d65729be84a8525124ecfb3499ca1
JAICHANGPARK <aristojeff@gmail.com>
2026-01-17 09:26:36 +0900

    Create horizontal_activity_card_widget.dart

---
+++ lib/ep2486_running_app/ui/widgets/horizontal_activity_card_widget.dart
@@ -0,0 +1,13 @@
+class HorizontalActivityCardWidget extends StatefulWidget {
+  const HorizontalActivityCardWidget({super.key});
+
+  @override
+  State<HorizontalActivityCardWidget> createState() => _HorizontalActivityCardWidgetState();
+}
+
+class _HorizontalActivityCardWidgetState extends State<HorizontalActivityCardWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
