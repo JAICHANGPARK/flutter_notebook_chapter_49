5d69115d3c5d019bea7f4a8e534fe0a53d18feb2
JAICHANGPARK <aristojeff@gmail.com>
2026-01-18 10:53:31 +0900

    Create task_app.dart

---
+++ lib/ep2490_task_management_app/task_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class TaskManagementApp extends StatelessWidget {
+  const TaskManagementApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
