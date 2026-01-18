17e8cd8bdd0aa60e5fd15ed7d2bf06a719beec94
JAICHANGPARK <aristojeff@gmail.com>
2026-01-18 10:55:21 +0900

    Update task_main_page.dart

---
+++ lib/ep2490_task_management_app/ui/task_main_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class TaskMainPage extends StatefulWidget {
+  const TaskMainPage({super.key});
+
+  @override
+  State<TaskMainPage> createState() => _TaskMainPageState();
+}
+
+class _TaskMainPageState extends State<TaskMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
