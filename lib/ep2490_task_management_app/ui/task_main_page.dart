0ad0723bd611ab28e5e3ff14d1d1fde4521e6b1a
JAICHANGPARK <aristojeff@gmail.com>
2026-01-19 18:28:29 +0900

    Update task_main_page.dart

--- lib/ep2490_task_management_app/ui/task_main_page.dart
+++ lib/ep2490_task_management_app/ui/task_main_page.dart
@@ -3,7 +3,6 @@ import 'package:hugeicons/hugeicons.dart';
 
 import 'task_home_page.dart';
 
-
 class TaskMainPage extends StatefulWidget {
   const TaskMainPage({super.key});
 
@@ -18,21 +17,16 @@ class _TaskMainPageState extends State<TaskMainPage> {
       backgroundColor: Colors.grey[200],
       body: IndexedStack(
         index: 0,
-        children: [
-          TaskHomePage(),
-          Placeholder(),
-          Placeholder(),
-          Placeholder()
-        ],
+        children: [TaskHomePage(), Placeholder(), Placeholder(), Placeholder()],
       ),
       bottomNavigationBar: Container(
         height: 100,
         decoration: BoxDecoration(
           borderRadius: .only(
             topLeft: Radius.circular(24),
-            topRight:  Radius.circular(24),
+            topRight: Radius.circular(24),
           ),
-              color: Colors.white
+          color: Colors.white,
         ),
         padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
         child: Row(
@@ -42,35 +36,35 @@ class _TaskMainPageState extends State<TaskMainPage> {
               mainAxisAlignment: .center,
               spacing: 4,
               children: [
-
                 HugeIcon(icon: HugeIcons.strokeRoundedHome01),
-                Text("Home")
+                Text("Home"),
               ],
             ),
-            Column(     mainAxisAlignment: .center,
+            Column(
+              mainAxisAlignment: .center,
               spacing: 4,
               children: [
                 HugeIcon(icon: HugeIcons.strokeRoundedHome01),
-                Text("Home")
+                Text("Home"),
               ],
             ),
-            CircleAvatar(
-              radius: 30,
-            ),
-            Column(     mainAxisAlignment: .center,
+            CircleAvatar(radius: 30),
+            Column(
+              mainAxisAlignment: .center,
               spacing: 4,
               children: [
                 HugeIcon(icon: HugeIcons.strokeRoundedHome01),
-                Text("Home")
+                Text("Home"),
               ],
             ),
-            Column(     mainAxisAlignment: .center,
+            Column(
+              mainAxisAlignment: .center,
               spacing: 4,
               children: [
                 HugeIcon(icon: HugeIcons.strokeRoundedHome01),
-                Text("Home")
+                Text("Home"),
               ],
-            )
+            ),
           ],
         ),
       ),
