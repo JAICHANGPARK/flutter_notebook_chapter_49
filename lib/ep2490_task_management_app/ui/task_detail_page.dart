03ba83098b1da3c42e8daf3e108121b2ad3f8779
JAICHANGPARK <aristojeff@gmail.com>
2026-01-23 18:22:03 +0900

    Update task_detail_page.dart

--- lib/ep2490_task_management_app/ui/task_detail_page.dart
+++ lib/ep2490_task_management_app/ui/task_detail_page.dart
@@ -13,6 +13,19 @@ class _TaskDetailPageState extends State<TaskDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300]!,
+      body: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                radius: 32,
+                child: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back_outlined)),
+              ),
+
+            ],
+          )
+        ],
+      ),
     );
 
   }
