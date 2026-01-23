ace49db2d5a42169f9e0800882b7681aa71ac468
JAICHANGPARK <aristojeff@gmail.com>
2026-01-23 18:15:36 +0900

    Update task_calendar_page.dart

--- lib/ep2490_task_management_app/ui/task_calendar_page.dart
+++ lib/ep2490_task_management_app/ui/task_calendar_page.dart
@@ -228,7 +228,7 @@ class _TaskCalendarPageState extends State<TaskCalendarPage> {
                     shape: RoundedSuperellipseBorder(
                       borderRadius: .circular(32),
                     ),
-                    color: .fromRGBO(229, 226, 248, 1),
+                    color: Colors.white,
                   ),
                   child: Column(
                     spacing: 16,
