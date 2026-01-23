1edb031094de3c70057bde4874267916994c7cd7
JAICHANGPARK <aristojeff@gmail.com>
2026-01-23 18:15:53 +0900

    Update task_calendar_page.dart

--- lib/ep2490_task_management_app/ui/task_calendar_page.dart
+++ lib/ep2490_task_management_app/ui/task_calendar_page.dart
@@ -190,24 +190,27 @@ class _TaskCalendarPageState extends State<TaskCalendarPage> {
                       ),
                       Text(
                         "Run every day to build stamina, improve health, and stay focused",
-                     ),
+                      ),
                       Row(
                         children: [
                           CircleAvatar(
                             radius: 24,
                             backgroundColor: Colors.white,
                           ),
-                          CircleAvatar( radius: 24,
+                          CircleAvatar(
+                            radius: 24,
                             backgroundColor: Colors.white,
                           ),
-                          CircleAvatar( radius: 24,
+                          CircleAvatar(
+                            radius: 24,
                             backgroundColor: Colors.white,
                           ),
-                          CircleAvatar( radius: 24,
+                          CircleAvatar(
+                            radius: 24,
                             backgroundColor: Colors.white,
                           ),
                         ],
-                      )
+                      ),
                     ],
                   ),
                 ),
@@ -252,17 +255,20 @@ class _TaskCalendarPageState extends State<TaskCalendarPage> {
                             radius: 24,
                             backgroundColor: .fromRGBO(229, 226, 248, 1),
                           ),
-                          CircleAvatar( radius: 24,
+                          CircleAvatar(
+                            radius: 24,
                             backgroundColor: .fromRGBO(229, 226, 248, 1),
                           ),
-                          CircleAvatar( radius: 24,
-                            backgroundColor:.fromRGBO(229, 226, 248, 1),
+                          CircleAvatar(
+                            radius: 24,
+                            backgroundColor: .fromRGBO(229, 226, 248, 1),
                           ),
-                          CircleAvatar( radius: 24,
-                            backgroundColor:.fromRGBO(229, 226, 248, 1),
+                          CircleAvatar(
+                            radius: 24,
+                            backgroundColor: .fromRGBO(229, 226, 248, 1),
                           ),
                         ],
-                      )
+                      ),
                     ],
                   ),
                 ),
