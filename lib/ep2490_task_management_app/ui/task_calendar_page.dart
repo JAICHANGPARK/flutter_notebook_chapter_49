654cabd4f56a3003ffb3e87913e4b307d6343ac0
JAICHANGPARK <aristojeff@gmail.com>
2026-01-23 17:33:58 +0900

    Update task_calendar_page.dart

--- lib/ep2490_task_management_app/ui/task_calendar_page.dart
+++ lib/ep2490_task_management_app/ui/task_calendar_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class TaskCalendarPage extends StatefulWidget {
+  const TaskCalendarPage({super.key});
+
+  @override
+  State<TaskCalendarPage> createState() => _TaskCalendarPageState();
+}
+
+class _TaskCalendarPageState extends State<TaskCalendarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
