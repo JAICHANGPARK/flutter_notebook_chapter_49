a880ac7a7a892dac58971766ec6a274438faf76d
JAICHANGPARK <aristojeff@gmail.com>
2026-01-23 18:24:15 +0900

    Update task_detail_page.dart

--- lib/ep2490_task_management_app/ui/task_detail_page.dart
+++ lib/ep2490_task_management_app/ui/task_detail_page.dart
@@ -15,42 +15,46 @@ class _TaskDetailPageState extends State<TaskDetailPage> {
       backgroundColor: Colors.grey[300]!,
       body: SafeArea(
         bottom: false,
-        child: Column(
-          children: [
-            Row(
-              children: [
-                CircleAvatar(
-                  radius: 32,
-                  child: IconButton(onPressed: (){
-                    Navigator.of(context).pop();
-                  }, icon: Icon(Icons.arrow_back_outlined)),
-                ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            spacing: 16,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 32,
+                    child: IconButton(onPressed: (){
+                      Navigator.of(context).pop();
+                    }, icon: Icon(Icons.arrow_back_outlined)),
+                  ),
 
-              ],
-            ),
-            Expanded(child: SingleChildScrollView(child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 200,
-                  child: Placeholder(),
-                ),
-                Text("title"),
-                Text("title"),
-                Container(
-                  height: 320,
-                  child: Placeholder(),
-                ),
-                Text("title"),
-                Container(
-                  height: 320,
-                  child: Placeholder(),
-                )
+                ],
+              ),
+              Expanded(child: SingleChildScrollView(child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 200,
+                    child: Placeholder(),
+                  ),
+                  Text("title"),
+                  Text("title"),
+                  Container(
+                    height: 320,
+                    child: Placeholder(),
+                  ),
+                  Text("title"),
+                  Container(
+                    height: 320,
+                    child: Placeholder(),
+                  )
 
 
-              ],
-            ),))
-          ],
+                ],
+              ),))
+            ],
+          ),
         ),
       ),
     );
