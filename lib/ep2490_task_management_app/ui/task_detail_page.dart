2dc504fd18085380728c6582a4c767b14b724dbe
JAICHANGPARK <aristojeff@gmail.com>
2026-01-26 17:20:32 +0900

    Update task_detail_page.dart

--- lib/ep2490_task_management_app/ui/task_detail_page.dart
+++ lib/ep2490_task_management_app/ui/task_detail_page.dart
@@ -134,8 +134,18 @@ class _TaskDetailPageState extends State<TaskDetailPage> {
                         ),
                       ),
                       Column(
-                          crossAxisAlignment: .start,
-                          children: [Text("Description"), Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")]),
+                        crossAxisAlignment: .start,
+                        spacing: 6,
+                        children: [
+                          Text("Description",style: TextStyle(
+                            fontSize: 18,
+                          ),),
+                          Text(
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                            maxLines: 3,
+                          ),
+                        ],
+                      ),
 
                       Container(height: 320, child: Placeholder()),
                       Text("title"),
