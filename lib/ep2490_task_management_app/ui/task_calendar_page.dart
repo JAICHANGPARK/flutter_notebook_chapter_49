14d7b4c603e7e1a6f524112fe3670212bf9875ea
JAICHANGPARK <aristojeff@gmail.com>
2026-01-28 19:14:34 +0900

    Update task_calendar_page.dart

--- lib/ep2490_task_management_app/ui/task_calendar_page.dart
+++ lib/ep2490_task_management_app/ui/task_calendar_page.dart
@@ -195,14 +195,20 @@ class _TaskCalendarPageState extends State<TaskCalendarPage> {
                       Text(
                         "Run every day to build stamina, improve health, and stay focused",
                       ),
-                      Stack(
-                        children: List.generate(4, (index){
-                          return CircleAvatar(
-                            radius: 24,
-                            backgroundColor: Colors.white,
-                          );
+                      Container(
+                        height: 52,
+                        child: Stack(
+                          children: List.generate(4, (index){
+                            return Positioned(
+                              left: index * 32,
+                              child: CircleAvatar(
+                                radius: 24,
+                                backgroundColor: Colors.white,
+                              ),
+                            );
 
-                        })
+                          })
+                        ),
                       ),
                     ],
                   ),
